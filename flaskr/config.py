a89bd7d03bae182fe6ffc49b1a4c270e7dcf7270
Toshio Abe@OfficeFarm <abtoc88@gmail.com>
2018-04-16 12:00:11 +0900

    データベース追加部分作成

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,3 +1,4 @@
+#SQLALCHEMY_DATABASE_URI = 'mysql://user:password@host/db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///./flaskr.db'
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- flaskr/models.py
+++ flaskr/models.py
@@ -7,10 +7,11 @@ def _get_now():
 class Entry(db.Model):
     __tablename__ = 'entres'
     id        = db.Column(db.Integer, primary_key=True)
+    title     = db.Column(db.Text)
+    text      = db.Column(db.Text)
     create_at = db.Column(db.DateTime, default =_get_now)
     update_at = db.Column(db.DateTime, onupdate=_get_now)
     def __repr__(self):
-        return '<Entry id={id}>'.format(
-            id=self.id
+        return '<Entry id={id} title={title}>'.format(
+            id=self.id, title=self.title
         )
-
