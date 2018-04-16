f9b05339fb293c99c6a5036c54a9c8bd3091b509
Toshio Abe@OfficeFarm <abtoc88@gmail.com>
2018-04-16 13:18:01 +0900

    編集、削除を追加

--- flaskr/views_entres.py
+++ flaskr/views_entres.py
@@ -17,7 +17,7 @@ def index():
     entres = Entry.query.all()
     return render_template('entres/index.pug', entres=entres)
 
-@bp.route('/create', methods=['GET','POST'])
+@bp.route('/create', methods=('GET','POST'))
 def create():
     form = EntryForm()
     if form.validate_on_submit():
@@ -27,8 +27,37 @@ def create():
         try:
           db.session.commit()
           flash('Entry created correctly.', 'success')
-          return redirect(url_for('index'))
+          return redirect(url_for('entres.index'))
         except:
           db.session.rollback()
           flash('Error generating entry!', 'danger')
     return render_template('entres/create.pug', form=form)
+
+@bp.route('/<id>/edit', methods=('GET','POST'))
+def edit(id):
+    entry = Entry.query.filter_by(id=id).first()
+    form = EntryForm(obj=entry)
+    if form.validate_on_submit():
+        entry = Entry()
+        form.populate_obj(entry)
+        db.session.add(entry)
+        try:
+          db.session.commit()
+          flash('Entry saved successfully.', 'success')
+          return redirect(url_for('entres.index'))
+        except:
+          db.session.rollback()
+          flash('Error update entry!', 'danger')
+    return render_template('entres/edit.pug', form=form)
+
+@bp.route('/<id>/destroy')
+def destroy(id):
+    entry = Entry.query.filter_by(id=id).first()
+    db.session.delete(entry)
+    try:
+        db.session.commit()
+        flash('Entry delete successfully.', 'success')
+    except:
+        db.session.rollback()
+        flash('Error delete entry!', 'danger')
+    return redirect(url_for('entres.index'))
