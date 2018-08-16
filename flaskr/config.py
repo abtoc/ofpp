3fb9858e497741e173698745becb40e889681b7a
Toshio Abe@OfficeFarm <abtoc88@gmail.com>
2018-08-16 10:44:51 +0900

    デバッグ

--- flaskr/config.py
+++ flaskr/config.py
@@ -8,4 +8,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # os.urandom(24)
 SECRET_KEY = '\xdcb#\xe2.\xbf4\xebHo\x85.\xcf\x9c\x1b\xbfR\xa5\x1e\xf1\x99.}^'
 # To instance/config.py
-#DEBUG = True
+DEBUG = True
