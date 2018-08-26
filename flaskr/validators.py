ce192fcba2ad3da0de50c7079ecb09e990ad0684
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-08-26 11:15:35 +0900

    カスタムバリデータをvalidators.pyに集約した

---
+++ flaskr/validators.py
@@ -0,0 +1,24 @@
+
+from wtforms.validators import Required, ValidationError
+
+class Unique(object):
+    def __init__(self, model, field, message='This element already exists.'):
+        self.model   = model
+        self.field   = field
+        self.message = message
+    def __call__(self, form, field):
+        check = self.model.query.filter(self.field == field.data).first()
+        if check:
+            raise ValidationError(self.message)
+
+class RequiredIf(Required):
+    def __init__(self, other_field_name, *args, **kwargs):
+        self.other_field_name = other_field_name
+        super(RequiredIf, self).__init__(*args, **kwargs)
+    def __call__(self, form, field):
+        other_field = form._fields.get(self.other_field_name)
+        if other_field is None:
+            raise Exception('no field named "%s" in form' % self.other_field_name)
+        if bool(other_field.data):
+            super(RequiredIf, self).__call__(form, field)
+
--- flaskr/views_persons.py
+++ flaskr/views_persons.py
@@ -7,30 +7,10 @@ from wtforms.validators import DataRequired, Required, Regexp, Optional, Validat
 from sqlalchemy    import func
 from flaskr        import db
 from flaskr.models import Person,WorkRec
+from flaskr.validators import Unique, RequiredIf
 from sqlalchemy.exc import IntegrityError
 
 bp = Blueprint('persons', __name__, url_prefix="/persons")
-
-class Unique(object):
-    def __init__(self, model, field, message='This element already exists.'):
-        self.model   = model
-        self.field   = field
-        self.message = message
-    def __call__(self, form, field):
-        check = self.model.query.filter(self.field == field.data).first()
-        if check:
-            raise ValidationError(self.message)
-
-class RequiredIf(Required):
-    def __init__(self, other_field_name, *args, **kwargs):
-        self.other_field_name = other_field_name
-        super(RequiredIf, self).__init__(*args, **kwargs)
-    def __call__(self, form, field):
-        other_field = form._fields.get(self.other_field_name)
-        if other_field is None:
-            raise Exception('no field named "%s" in form' % self.other_field_name)
-        if bool(other_field.data):
-            super(RequiredIf, self).__call__(form, field)
 
 class PersonForm(FlaskForm):
     name = StringField('名前', validators=[
