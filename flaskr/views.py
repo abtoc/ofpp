ae3bb837d9c231daae33caba87356ef218e962c5
Toshio Abe@OfficeFarm <abtoc88@gmail.com>
2018-08-28 11:24:09 +0900

    本日の勤怠状況をトップ画面に表示

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,11 +1,31 @@
+from datetime      import date
 from flask         import request, redirect, url_for, render_template, flash
 from flaskr        import app,db
-from flaskr.models import Person
+from flaskr.models import Person, WorkRec
 
 @app.route('/')
 def index():
+    today = date.today()
     persons = Person.query.filter_by(enabled=True).order_by(Person.name.desc()).all()
-    return render_template('index.pug', persons=persons)
+    items = []
+    for person in persons:
+        item = {}
+        item['id'] = person.id
+        item['name'] = person.display
+        workrec = WorkRec.get_date(person.id, today)
+        item['work_in'] = ''
+        item['work_out'] = ''
+        if workrec is not None:
+            if workrec.work_in is not None:
+                item['work_in'] = workrec.work_in
+            elif workrec.situation is not None:
+                item['work_in'] = workrec.situation
+            if workrec.work_out is not None:
+                item['work_out'] = workrec.work_out
+            elif workrec.reason is not None:
+                item['work_out'] = workrec.reason
+        items.append(item)
+    return render_template('index.pug', items=items)
     #return redirect(url_for('persons.index'))
 
 @app.after_request
